d95f0153045164e1cf4342b8f102bba5ddecef38
Gerard Pelly <gerardwp@gmail.com>
2019-09-29 19:39:30 +1000

    js chart working

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,3 +6,41 @@ $(document).ready(function() {
     $("div.menu").toggleClass("menu-clicked");
   });
 });
+
+new Chart(document.getElementById("doughnut-chart"), {
+  type: "doughnut",
+  data: {
+    labels: ["Food", "Drink", "Clothing", "Treats", "Drugs"],
+    datasets: [
+      {
+        label: "Spending",
+        backgroundColor: [
+          "#0a998b",
+          "#2c2e47",
+          "#ffff7a",
+          "#ff9b6c",
+          "#ea4651"
+        ],
+        data: dataArray, // the data
+        borderColor: "##eee",
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Your Spending",
+      fontColor: "#01c46f",
+      fontSize: 14
+    },
+    legend: {
+      display: true,
+      position: "top",
+      fontSize: 13,
+      labels: {
+        fontColor: "white"
+      }
+    }
+  }
+});
