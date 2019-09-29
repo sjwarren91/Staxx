fa8c0d1aa21cf055c2d000a8ec25ff3c3dd45d75
Gerard Pelly <gerardwp@gmail.com>
2019-09-29 23:49:20 +1000

    added area for adding new transaction - input is auto formatting as you type, inputting commas and stops to display the monetary amount in a nice way. value is then gathered

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -44,3 +44,76 @@ new Chart(document.getElementById("doughnut-chart"), {
     }
   }
 });
+
+// Getting the transaction values, minus the $ sign and the commas
+
+$("input[data-type='currency']").on({
+  keyup: function() {
+    formatCurrency($(this));
+  },
+  blur: function() {
+    formatCurrency($(this), "blur");
+  }
+});
+
+function formatNumber(n) {
+  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var transactionArray = [];
+
+function formatCurrency(input, blur) {
+  var inputVal = input.val();
+
+  transactionArray.push(inputVal); // pushes current val of input to transactionArray
+
+  // don't validate empty input
+  if (inputVal === "") {
+    return;
+  }
+  var originalLen = inputVal.length;
+  var caretPos = input.prop("selectionStart");
+
+  if (inputVal.indexOf(".") >= 0) {
+    var decimalPos = inputVal.indexOf(".");
+    var leftSide = inputVal.substring(0, decimalPos);
+    var rightSide = inputVal.substring(decimalPos);
+    leftSide = formatNumber(leftSide);
+
+    rightSide = formatNumber(rightSide);
+
+    if (blur === "blur") {
+      rightSide += "00";
+    }
+
+    rightSide = rightSide.substring(0, 2);
+    inputVal = "$" + leftSide + "." + rightSide;
+  } else {
+    inputVal = formatNumber(inputVal);
+    inputVal = "$" + inputVal;
+    if (blur === "blur") {
+      inputVal += ".00";
+    }
+  }
+
+  input.val(inputVal);
+
+  var updatedLen = inputVal.length;
+  caretPos = updatedLen - originalLen + caretPos;
+  input[0].setSelectionRange(caretPos, caretPos);
+}
+
+$("#paymentSubmit").on("click", function(event) {
+  event.preventDefault();
+
+  if (transactionArray.length > 0) {
+    var amount = transactionArray[transactionArray.length - 1];
+    amount = amount.replace(/[$,]/g, "");
+  } else {
+    return;
+  }
+
+  console.log(amount);
+
+  //amount is the value of the trasnaction with $ and commas removed.
+});
