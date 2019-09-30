92c231f9fa814d9f9f1e1e80426f088f18531ed0
Gerard Pelly <gerardwp@gmail.com>
2019-09-30 15:01:17 +1000

    added handlbars for transactions and notifications lists - js handlebars and css for header menu

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $("#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4").click(function() {
     $(this).toggleClass("open");
     $("div.menu").toggleClass("menu-clicked");
+    $("#header-menu").toggleClass("header-menu-clicked");
   });
 });
 
